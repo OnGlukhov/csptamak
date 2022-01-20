04a150a03a21502dbbe72fab354ce610fd159fe2
Oleg Glukhov <o.gluhov2017@yandex.ru>
2022-01-20 15:46:22 +0300

    add animation firstScreen

--- components/FirstScreen/FirstScreen.js
+++ components/FirstScreen/FirstScreen.js
@@ -1,22 +1,42 @@
 import styles from './FirstScreen.module.sass'
 import { Link } from 'react-scroll'
+import { motion } from 'framer-motion'
+import { animations } from '../../styles/animations'
 
 export default function FirstScreen() {
 	return (
 		<section className={styles.firstScreen} id='main'>
 			<div className={styles.firstScreen_wrapper_block}>
 				<div className={styles.firstScreen_block}>
-					<div className={`${styles.firstScreen_line} ${styles.fadeInLeft}`}></div>
+					<motion.div
+						// className={`my-4 bg-belplit24`}
+						initial='initial'
+						animate='animate'
+						exit='exit'
+						variants={animations.line.variants}
+						transition={animations.line.transition} className={`${styles.firstScreen_line} ${styles.fadeInLeft}`}></motion.div>
 
-					<h1 className={styles.firstScreen_title}>
+					<motion.h1
+						initial='initial'
+						animate='animate'
+						exit='exit'
+						variants={animations.slideUp.variants}
+						transition={animations.slideUp.transition}
+						className={styles.firstScreen_title}>
 						<span className={styles.firstScreen_span}>ЦСП</span> ТАМАК от 724
 						руб.
-					</h1>
-					<p className={styles.firstScreen_subtitle}>
+					</motion.h1>
+					<motion.p
+						initial='initial'
+						animate='animate'
+						exit='exit'
+						variants={animations.slideUp.variants}
+						transition={animations.slideUp.transition}
+						className={styles.firstScreen_subtitle}>
 						ЦСП (Цементно-стружечные плиты) ТАМАК - это материал нового
 						поколения. Он прочный, влагостойкий и долговечный, с плотностью
 						1250-1400кг/м3, толщиной 8-36 мм, гладкой и твердой поверхностью!
-					</p>
+					</motion.p>
 					<Link
 						to="contacts"
 						spy={true}
